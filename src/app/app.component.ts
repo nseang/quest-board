c244b41c8555333e4da6a2905c703a8f5f50cec4
Narith Seang <nseang2@gmail.com>
2023-06-17 17:53:48 -0400

    initial auth work

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FirebaseUISignInFailure, FirebaseUISignInSuccessWithAuthResult, FirebaseuiAngularLibraryService } from 'firebaseui-angular';
+import { QuestReceptionistService } from './quest-receptionist.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'quest-board';
+
+  constructor(
+    private questService: QuestReceptionistService,
+    private firebaseuiAngularLibraryService: FirebaseuiAngularLibraryService
+  ) {
+    firebaseuiAngularLibraryService.firebaseUiInstance.disableAutoSignIn();
+   }
+
+  successCallback(signInSuccessData: FirebaseUISignInSuccessWithAuthResult) {
+    // TODO
+  }
+    
+  errorCallback(errorData: FirebaseUISignInFailure) {
+    // TODO
+  }
+    
 }
